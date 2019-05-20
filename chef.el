5c783298c1a6bb71f3add0cd5b0bf148bcdd4409
Christiaan Janssen <christiaanjanssen@outlook.com>
2019-05-20 11:50:39 +0200

    Run chef commands from recipe root

--- chef.el
+++ chef.el
@@ -4,31 +4,54 @@
 
 ;;; Code:
 
+(defvar test-dir (buffer-file-name))
+
+(defun pwd! ()
+  "Get the current working dir as string."
+  (cadr (split-string (pwd))))
 
 (defun parent-directory (dir)
-  "return parent directory of dir."
+  "Return parent directory of DIR."
   (unless (equal "/" dir)
     (file-name-directory (directory-file-name dir))))
 
 (defun run-command-in-parrent-dir (command)
+  "Run COMMAND in the parrent dir."
   (cd (parent-directory
        (file-name-directory (buffer-file-name))))
   (async-shell-command command))
 
+(defun get-recipe-root (path)
+  "Find the project root for PATH."
+  (cd path)
+  (if (file-exists-p (concat (pwd!) ".kitchen.yml"))
+      (pwd!)
+    (get-project-root (parent-directory (pwd!)))))
+
+(defun run-command-in-recipe-root (command)
+  "Run COMMAND in the recipe root folder."
+  (cd (get-project-root (file-name-directory (buffer-file-name))))
+    (async-shell-command command))
+
 (defun kitchen-converge ()
   "Run chef test kitchen converge."
   (interactive)
-  (run-command-in-parrent-dir "kitchen converge"))
+  (run-command-in-recipe-root "kitchen converge"))
 
 (defun kitchen-test ()
   "Run chef test kitchen test."
   (interactive)
-  (run-command-in-parrent-dir "kitchen test"))
+  (run-command-in-recipe-root "kitchen test"))
 
 (defun kitchen-destroy ()
   "Run chef test kitchen destory."
   (interactive)
-  (run-command-in-parrent-dir "kitchen destroy"))
+  (run-command-in-recipe-root "kitchen destroy"))
+
+(defun kitchen-verify ()
+  "Run chef test kitchen destory."
+  (interactive)
+  (run-command-in-recipe-root "kitchen verify"))
 
 (defun knife-boostrap ()
   "Bootstrap a new node using chef."
